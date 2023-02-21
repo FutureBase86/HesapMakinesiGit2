8ddce9ce70448bdc8cbec8a25654fb5084772f19
sedat.akkus <sedat.akkus@sabancidx.com>
2023-02-21 21:50:27 +0300

    çarpma özelliği eklendi

--- Program.cs
+++ Program.cs
@@ -22,6 +22,10 @@
             {
                 Console.WriteLine("Sonuc: " + (sayi1 - sayi2) );
             }
+            else if (islem == "*")
+            {
+                Console.WriteLine("Sonuc: " + (sayi1 * sayi2));
+            }
 
         }
     }
