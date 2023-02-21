72600ffc55668690cc178ca38b92cec8eb6ea6ab
sedat.akkus <sedat.akkus@sabancidx.com>
2023-02-21 21:50:55 +0300

    bölme özelliği eklendi

--- Program.cs
+++ Program.cs
@@ -26,6 +26,10 @@
             {
                 Console.WriteLine("Sonuc: " + (sayi1 * sayi2));
             }
+            else if (islem == "/")
+            {
+                Console.WriteLine("Sonuc: " + (sayi1 / sayi2));
+            }
 
         }
     }
